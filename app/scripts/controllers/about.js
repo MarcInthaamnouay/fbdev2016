84df537386568808ab941aa8d608d1ac580ec205
timrr9 <tmolavy@yahoo.fr>
2016-11-14 12:54:29 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fbdevApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fbdevApp
+ */
+angular.module('fbdevApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fbdevApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fbdevApp
+ */
+angular.module('fbdevApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
