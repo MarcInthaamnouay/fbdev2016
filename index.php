b8efbbad0d468f3a38a3342d38c5fdbfa54dc9bc
MarcusMini <marc.inthaamnouay@gmail.com>
2016-12-05 18:41:33 +0100

    resolving branch issue

--- index.php
+++ index.php
@@ -1,186 +1,61 @@
 <?php
-	require_once __DIR__ . '/fb_sdk/src/Facebook/autoload.php';
 
-	session_start();
- 	$messageError= array();
- 	$permissions = ['email','manage_pages','publish_pages', 'user_photos', 'user_videos', 'page_show_list'];
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-	$fb = new Facebook\Facebook([
-	  'app_id' => '639819622867674',
-	  'app_secret' => '505ddf28d58518809be301ac1e732393',
-	  'default_graph_version' => 'v2.8',
-	  'default_access_token' => isset($_SESSION['facebook_access_token']) ? $_SESSION['facebook_access_token'] : '639819622867674s|505ddf28d58518809be301ac1e732393'
-	]);
-  	
-  	// Récupération du nom utilisateur
-	try {
-  		$response = $fb->get('/me?fields=id,name,link,gender,locale,picture');
-  		$user = $response->getGraphUser();
-  		$userName = $user['name'];
-  		$userID = $user['id'];
-  		$userLink = $user['link'];
-  		$userGender = $user['gender'];
-  		$userLocale = $user['locale'];
-  		$userPicture = $user['picture'];
-  		$userAccessToken = $_SESSION['facebook_access_token'];
-  		$isConnect = true;
+session_start();
 
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		$isConnect = false;
-  		array_push($messageError,'Graph returned an error: ' . $e->getMessage());
-		} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		$isConnect = false;
-  		array_push($messageError,'Facebook SDK returned an error: ' . $e->getMessage());
-	}
-	
-	
-	//get login URL
-	$helper = $fb->getRedirectLoginHelper();
-	$loginUrl = $helper->getLoginUrl('http://berseck.fbdev.fr/fb/login-callback.php', $permissions);
-	$rerequestUrl = $helper->getReRequestUrl('http://berseck.fbdev.fr/fb/login-callback.php', $permissions);
-	$logoutUrl = $helper->getLogoutUrl('http://berseck.fbdev.fr/fb/logout.php', $permissions);
+// Using slim framework to handle all the APP and mainly the API.
 
-	//Now if we are connect
-	if($userName != null) echo "Bienvenue ". $userName;
-	else echo '<a href="'.$loginUrl.'"> Login </a><br />';
+include('vendor/autoload.php');
+require_once __DIR__ . '/Autoload/autoload.php';
+require_once __DIR__ . '/Services/getphoto.php';
 
-	//get photos
-    $pagesEdge = $fb->get('/me/photos?access_token=' . $_SESSION['facebook_access_token'])->getGraphEdge();
-   
-   	
-    
-    //Check permissions
-    $lackPermission=false; //On suppose qu'au départ les permissions sont mauvaises.
+// Configure the framework to show the error in DEV
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
 
-	try {
-	    $response = $fb->get('/me/permissions');
-	    $perms = $response->getDecodedBody();
+// Slim app
 
-        foreach($permissions AS $myPerm){//Pour chacune de mes perms, je vérifie que l'utilisateur à la perm
-        	foreach($perms['data'] AS $perm)
-            	if($perm['permission'] == $myPerm && $perm['status']!='granted') { $lackPermission = true; break; }
-        }
+$app = new Slim\App(["settings" => $config]);
 
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	    array_push($messageError,"Erreur facebook : ".$e->getMessage());
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	    array_push($messageError,"erreur SDlL ".$e->getMessage());
-	}
+$app->post('/api/v1.0/albums/', function($request, $response, $args){
+  //  $app = \Slim\Slim::getInstance();
+  // Get the list of the albums
+  $data = $request->getParsedBody();
+  $token = filter_var($data['token']);
 
-	//Check albums
-	try{
-    	$res = $fb->get('/me?fields=id,picture');
-    	$usr = $res->getGraphUser();
-    	$userID = $usr['id'];
-    		if(isset($userID)){
-      			$fbApp = new Facebook\FacebookApp('639819622867674', '505ddf28d58518809be301ac1e732393');
-      			$request = new Facebook\FacebookRequest($fbApp, $_SESSION['facebook_access_token'] , 'GET', '/'.$userID.'/albums');
-      try{
-        $response = $fb->getClient()->sendRequest($request);
- 
-        $resBody = $response->getDecodedBody();
-     	echo '<br />
-     		<form>
-				<select name="albums" size="1">';
+  if($token){
+    // Call the getListOfAlbums function by passing the fb variable
+    $data = getListOfAlbums($token);
+    $newdata = $response->withJson($data, 201);
 
-        foreach($resBody as $key => $value){
-          foreach($value as $v){
-          	if(isset($v["name"]))
-            echo '<option value='.$v["id"].'>'.$v["name"].'</option>';
-          }
-        }
-        	echo '<select>
-        	</form>';
-
- 
-      } catch(Facebook\Exceptions\FacebookResponseException $e){
-        echo $e->getMessage();
-      }
-    }
- 
-  } catch(Facebook\Exceptions\FacebookResponseException $e){
-    echo "<p>An error happened while getting you're photo ".$e."</p>";
+    return $newdata;
+  } else {
+    return $response->withJson(array('nodata' => 'no data'), 404);
   }
+});
 
-	//Affichage
-	if($lackPermission && $userName != null) // connecté avec manque de permission
-		echo '<a href="'.$loginUrl.'"> Login </a><br />';
-	else if(!$lackPermission && $userName != null)// connecté sans manque de permission
-		echo '<a href="'.$logoutUrl.'"> Logout </a><br />';
-	else echo '<a href="'.$rerequestUrl.'"> Login </a><br />';
+$app->post('/api/v1.0/photos/', function($request, $response, $args){
+  $data = $request->getParsedBody();
+  $token = filter_var($data['token']);
+  $id = filter_var($data['albumID']);
 
-/*public function checkAccessToken()
-{
-	if (empty($_SESSION['facebook_access_token'])) return false;
-	try {
-			$reponse = $fb->get('/debug_token?input_token'.$_SESSION['facebook_access_token']);
-			$graphObject = $response->getGraphObject();
-		}
-		catch(Exception $e) {
-			return false;
-		}
-	return true;
-}
-*/
+  $photos = getListOfPhotosFromAlbum($id, $token);
 
-//Check photos
-	try{
-    	$albumID = "1378638042376891";
-    		if(isset($albumID)){
-      			$fbApp = new Facebook\FacebookApp('639819622867674', '505ddf28d58518809be301ac1e732393');
-      			$request = new Facebook\FacebookRequest($fbApp, $_SESSION['facebook_access_token'] , 'GET', '/'.$albumID.'/photos/uploaded?fields=source,images,name');
-      try{
-        $response = $fb->getClient()->sendRequest($request);
- 
-        $resBody = $response->getDecodedBody();
-     	echo 'Affichage de l"album : '.$albumID.'<br />';
+  return $response->withJson($photos, 201);
+});
 
-        foreach($resBody as $key => $value){
-          foreach($value as $v){
-          	if(isset($v["id"]))
-          	{
-          		if(isset($v["name"])) $name = $v["name"];
-          		else $name = 'Pas de nom';
-          		echo '<img src="'.$v["source"].'" width =50px/><br /> Name : '.$name.'<br />';
-          	}
-          }
-        }
+$app->get('/api/v1.0/crons', function($request, $response, $args){
 
- 
-      } catch(Facebook\Exceptions\FacebookResponseException $e){
-        echo $e->getMessage();
-      }
-    }
- 
-  } catch(Facebook\Exceptions\FacebookResponseException $e){
-    echo "<p>An error happened while getting you're photo ".$e."</p>";
-  }
+});
 
+$app->get('/api/v1.0/contest/{id}', function($request, $response, $args){
 
-  //upload photo
-  
-  try {
-  	$file = "test.JPG";
-  	$ch = curl_init();
-	$args = array(
-	   'message' => 'Photo from application',
-	);
-	$args[basename($file)] = '@' . realpath($file);
-	$ch = curl_init();
-	$url = 'http://graph.facebook.com/'.$albumID.'/photos?access_token='.$_SESSION['facebook_access_token'];
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
-	$data = curl_exec($ch);
-	//returns the photo id
-	print_r(json_decode($data,true));
-  } catch(FacebookRequestException $e) {
+});
 
-    echo "Exception occured, code: " . $e->getCode();
-    echo " with message: " . $e->getMessage();
+$app->post('/api/v1.0/contest/create', function($request, $response, $args){
 
-  }   
+});
 
-?>
+$app->run();
