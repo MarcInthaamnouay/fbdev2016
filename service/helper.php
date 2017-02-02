26ebab5e565df88c075daf1ef48c01492a506cc5
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-02-03 00:07:09 +0100

    hotfix

--- service/helper.php
+++ service/helper.php
@@ -67,21 +67,47 @@ Class Helper{
      *  @return token string
      *  @return error message if PDOException
      */
+    // public static function retrieveToken($userID){
+    //      $db = new Db();
+    //     try{
+    //         $con = $db->connect();
+    //         $results = $con->query("SELECT * FROM user_trace WHERE id_user = ".$userID);
+    //         $result = $results->fetch();
+    //         $token = '';
+
+    //         foreach($result as $key => $value){
+    //             if($key == 'token'){
+    //                 $token = $value;
+    //             }
+    //         }
+
+    //         return $token;
+    //     } catch(PDOException $e){
+    //         return $e;
+    //     }
+
+        
+    // }
+
     public static function retrieveToken($userID){
          $db = new Db();
         try{
             $con = $db->connect();
-            $results = $con->query("SELECT * FROM user_trace WHERE id_user = ".$userID);
-            $result = $results->fetch();
-            $token = '';
+            $stmt = $con->prepare("SELECT * FROM user_trace WHERE id_user = :userID");
+            $stmt->bindParam(":userID", $userID);
+            $stmt->execute();
 
-            foreach($result as $key => $value){
-                if($key == 'token'){
-                    $token = $value;
-                }
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $token = false;
+   
+            if($result['token']){
+                $token = $result['token'];
+                return $token;
             }
+                
+             if(!$token)
+                 return 'error';
 
-            return $token;
         } catch(PDOException $e){
             return $e;
         }
